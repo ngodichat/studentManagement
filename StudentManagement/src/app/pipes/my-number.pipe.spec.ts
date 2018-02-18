0f50f1364753952241bb8fe38d6bf810961af927
phamnguyenbinh <phamnguyenbinh90@gmail.com>
2018-02-18 21:36:01 +0700

    Refactor

--- StudentManagement/src/app/pipes/my-number.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyNumberPipe } from './my-number.pipe';
-
-describe('MyNumberPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MyNumberPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
